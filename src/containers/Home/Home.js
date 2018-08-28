14dc50c30f90cefba93af7c9117f8d9db93e3648
mly <ly.meng@Ctrip.com>
2018-08-28 20:19:46 +0800

    modified:   package.json 	new file:   src/components/calendar/agenda/img/knob@2x.png 	new file:   src/components/calendar/agenda/index.js 	new file:   src/components/calendar/agenda/platform-style.ios.js 	new file:   src/components/calendar/agenda/platform-style.js 	new file:   src/components/calendar/agenda/reservation-list/index.js 	new file:   src/components/calendar/agenda/reservation-list/reservation.js 	new file:   src/components/calendar/agenda/reservation-list/style.js 	new file:   src/components/calendar/agenda/style.js 	new file:   src/components/calendar/calendar-list/index.js 	new file:   src/components/calendar/calendar-list/item.js 	new file:   src/components/calendar/calendar-list/style.js 	new file:   src/components/calendar/calendar/day/basic/index.js 	new file:   src/components/calendar/calendar/day/basic/style.js 	new file:   src/components/calendar/calendar/day/custom/index.js 	new file:   src/components/calendar/calendar/day/custom/style.js 	new file:   src/components/calendar/calendar/day/multi-dot/index.js 	new file:   src/components/calendar/calendar/day/multi-dot/style.js 	new file:   src/components/calendar/calendar/day/multi-period/index.js 	new file:   src/components/calendar/calendar/day/multi-period/style.js 	new file:   src/components/calendar/calendar/day/period/index.js 	new file:   src/components/calendar/calendar/day/period/style.js 	new file:   src/components/calendar/calendar/header/index.js 	new file:   src/components/calendar/calendar/header/style.js 	new file:   src/components/calendar/calendar/img/next.png 	new file:   src/components/calendar/calendar/img/next@1.5x.android.png 	new file:   src/components/calendar/calendar/img/next@2x.android.png 	new file:   src/components/calendar/calendar/img/next@2x.ios.png 	new file:   src/components/calendar/calendar/img/next@3x.android.png 	new file:   src/components/calendar/calendar/img/next@4x.android.png 	new file:   src/components/calendar/calendar/img/previous.png 	new file:   src/components/calendar/calendar/img/previous@1.5x.android.png 	new file:   src/components/calendar/calendar/img/previous@2x.android.png 	new file:   src/components/calendar/calendar/img/previous@2x.ios.png 	new file:   src/components/calendar/calendar/img/previous@3x.android.png 	new file:   src/components/calendar/calendar/img/previous@4x.android.png 	new file:   src/components/calendar/calendar/index.js 	new file:   src/components/calendar/calendar/style.js 	new file:   src/components/calendar/calendar/updater.js 	new file:   src/components/calendar/component-updater.js 	new file:   src/components/calendar/dateutils.js 	new file:   src/components/calendar/index.js 	new file:   src/components/calendar/input.js 	new file:   src/components/calendar/interface.js 	new file:   src/components/calendar/style.js 	new file:   src/components/calendar/testIDs.js 	modified:   src/containers/Home/Home.js 	new file:   src/styles/styleVars.js 	new file:   utils/dateutils.js 	new file:   utils/timeTransform.js

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,28 +1,54 @@
-// import React, { Component } from 'react'
-// import { Text, View } from 'react-native'
-// import { Button } from 'antd-mobile-rn';
-
-// export default class Home extends Component {
-//   render() {
-//     return (
-//       <View> 
-//         <Button>
-//           Start
-//         </Button>
-//       </View>
-//     )
-//   }
-// }
-
-
 import React, { Component } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 import { Button, Carousel } from 'antd-mobile-rn';
+import { CalendarList, LocaleConfig } from '../../components/calendar';
+LocaleConfig.locales['fr'] = {
+  monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
+  monthNamesShort: ['Janv.','Févr.','Mars','Avril','Mai','Juin','Juil.','Août','Sept.','Oct.','Nov.','Déc.'],
+  dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+  dayNamesShort: ['日','一','二','三','四','五','六']
+};
+
+LocaleConfig.defaultLocale = 'fr';
+
+
+
+
 
 
 export default class Home extends Component {
   state = {
     list: [1,2,3]
+  }
+
+  checkFestival = (data) => {
+    const festivalConfig = [
+      {"2017-01-01": "元旦"},
+      {"2018-01-01": "元旦"},
+      {"2017-01-28": "春节"},
+      {"2017-02-14": "情人节"},
+      {"2018-02-14": "情人节"},
+      {"2017-02-11": "元宵"},
+      {"2017-04-04": "清明"},
+      {"2017-05-01": "劳动节"},
+      {"2018-05-01": "劳动节"},
+      {"2017-06-01": "儿童节"},
+      {"2018-06-01": "儿童节"},
+      {"2017-05-30": "端午"},
+      {"2017-09-10": "教师节"},
+      {"2018-09-10": "教师节"},
+      {"2017-10-01": "国庆"},
+      {"2017-10-04": "中秋节"},
+      {"2018-10-01": "国庆"},
+      {"2017-12-25": "圣诞"},
+      {"2018-12-25": "圣诞}"}
+    ]
+    festivalConfig.map((item, index) => {
+      if(item){}
+      // return (<View>
+
+      // </View>)
+    })
   }
 
   render() {
@@ -37,22 +63,30 @@ export default class Home extends Component {
           afterChange={this.onHorizontalSelectedIndexChange}
         >
           {this.renderBanner(list)}
-          {/* <View style={[styles.containerHorizontal, { backgroundColor: 'red' }]}>
-            <Text>Carousel 1</Text>
-          </View>
-          <View style={[styles.containerHorizontal, { backgroundColor: 'blue' }]}>
-            <Text>Carousel 2</Text>
-          </View>
-          <View style={[styles.containerHorizontal, { backgroundColor: 'yellow' }]}>
-            <Text>Carousel 3</Text>
-          </View>
-          <View style={[styles.containerHorizontal, { backgroundColor: 'aqua' }]}>
-            <Text>Carousel 4</Text>
-          </View>
-          <View style={[styles.containerHorizontal, { backgroundColor: 'fuchsia' }]}>
-            <Text>Carousel 5</Text>
-          </View> */}
         </Carousel>
+        <CalendarList
+          monthFormat={'yyyy MM'}
+          // 当滚动视图中可见的月份发生变化时执行的回调. Default = undefined
+          onVisibleMonthsChange={(months) => {console.log('now these months are visible', months);}}
+          // 允许滚动到过去的最大月份. Default = 50
+          pastScrollRange={1}
+          // 允许滚动到未来的最大月份. Default = 50
+          futureScrollRange={1}
+          // 启用或禁用日历列表滚动
+          scrollEnabled={true}
+          // 启用或禁用垂直滚动指示器. Default = false
+          showScrollIndicator={true}
+          // ...calendarParams
+          dayComponent={({date, state}) => {
+            let otherMsg = ''
+            this.checkFestival(date)
+            return (
+            <View style={{flex: 1}}>
+              <Text>{date.day}</Text>
+              { !!otherMsg && <Text>{otherMsg}</Text> }
+            </View>);
+          }}
+        />
       </View>
     )
   }
