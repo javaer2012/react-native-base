18459b2699d30768b40ffa4885f939e70517798a
menglingyu <lingyu.meng@baishancloud.com>
2018-08-29 09:09:27 +0800

    modified:   App.js 	modified:   src/containers/Home/Home.js 	new file:   src/mock/home.js

---
+++ src/mock/home.js
@@ -0,0 +1,30 @@
+export const bannerNav_mock = {
+      "errcode": 1,
+      "errmsg": "ok",
+      "bannerList": [
+        {
+          "id": "1",
+          "imgPath": "https://域名/source/images/banner.jpg",
+          "linkUrl": "/pages/index/TV/TV"
+        },
+        {
+          "id": "2",
+          "imgPath": "https://域名/source/images/iPhone-x.jpg",
+          "linkUrl": "/pages/index/phoneList/phoneList"
+        }
+      ],
+      "navList": [
+        {
+          "id": "11",
+          "navTitle": "信用租机",
+          "iconPath": "https://域名/source/images/xy.png",
+          "linkUrl": "/pages/index/phoneList/phoneList"
+        },
+        {
+          "id": "22",
+          "navTitle": "电视机",
+          "iconPath": "https://域名/source/images/dhf.png",
+          "linkUrl": "/pages/index/TV/TV"
+        }
+      ]
+}
