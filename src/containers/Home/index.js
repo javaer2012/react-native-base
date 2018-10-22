ca5264017b95915f7bf2623033eddf2f1ffe9de6
menglingyu <lingyu.meng@baishancloud.com>
2018-10-23 00:15:39 +0800

    ui fixed

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -27,6 +27,14 @@ class Home extends RentApp {
       >
         二维码
       </Button>),
+      headerRight: (  //定义导航栏右侧的按钮
+      <Text
+        style={{ fontSize: 12, color: '#fff' }}
+        containerStyle={{ marginRight: 10, height: 30, width: 50, overflow: 'hidden', borderRadius: 4, backgroundColor: '#343434', borderColor: '#b2b2b2', borderWidth: 1, justifyContent: 'center' }}
+        onPress={() => contact()}
+      >
+          <Image source={ require('../../images/home/icon_share.png')} />
+      </Text>),
     }
 
   state = {
